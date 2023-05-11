ff2fe7d484e873ab3d07ca4986e1344850814ea9
Rosiee7 <88431787+Rosiee7@users.noreply.github.com>
2023-05-11 16:54:25 +0300

    Add files via upload

---
+++ weather-app/src/App.js
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import './App.css';
+import Weather from './components/Weather';
+
+
+function App() {
+
+    const [data, setData] = useState([{}]);
+    const [city, setCity] = useState("");
+    const apiKey = '4d6cea2fa6646232ecab13eea891d823';
+
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+   
+    function handleChange(event) {
+        setCity(event.target.value);
+    }
+
+    function onSubmitHandler (event){
+        event.preventDefault();
+
+        fetch(apiUrl)
+            .then((response) => response.json())
+            .then((data) => setData(data)).then(() => console.log(data)).then(event.target.reset());
+        console.log(data.weather[0].icon);
+    }
+
+  return (
+      <div className="App">
+
+          <form className="searchForm" onSubmit={onSubmitHandler}>
+              <div className="searchInput"><input id="cityName" htmlFor="cityName" placeholder="Enter City..." onChange={handleChange} type="text" /> <input className="submit" type="submit" value="Send" /></div>
+          </form>
+
+          {typeof data.main !== 'undefined' ? <Weather data={data} /> : <p className="cityNotFound">City not found</p>
+          }
+
+    </div>
+  );
+}
+
+export default App;
---
+++ weather-app/src/components/Weather.js
@@ -0,0 +1,38 @@
+import '../App.css';
+import moment from 'moment'
+
+
+function Weather({data}) {
+
+    function getTime(timezone, sunTime) {
+
+        let x = moment.utc(sunTime, 'X').add(timezone, 'seconds').format('HH:mm a');
+
+        return x;
+    }
+
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    return (
+            <div className="WeatherInfo" >
+            <p>{data.name} , {capitalizeFirstLetter(data.sys.country)}</p>
+            <p>{capitalizeFirstLetter(data.weather[0].description)}</p>
+            <img src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} alt="Weather img" />
+            <p>{Math.round(data.main.temp - 273.15)}{'\u00b0'}</p>
+            <p>Sunrise :{getTime(7200, data.sys.sunrise)} (IST)</p>
+            <p>Sunset : {getTime(7200, data.sys.sunset)} (IST) </p>
+            {
+                    /*<p>{getTime(0, data.sys.sunrise)}(GMT)</p>
+                    <p>{getTime(0, data.sys.sunset)} (GMT)</p>
+                    <p>{getTime(-18000, data.sys.sunrise)}Eastern Time Zone (UTC-05: 00) </p>
+                    <p>{getTime(-18000, data.sys.sunset)}Eastern Time Zone (UTC-05:00)</p>*/
+            }
+            </div> 
+            
+
+    );
+}
+
+export default Weather;     
